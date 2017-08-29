1d83eb79bf156a7ec0066ec2a2bff1a10b8b2744
sirirak <hotmail22866@gmail.com>
2017-08-29 09:40:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/minato/kotlinsimpleappfindmyage/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.minato.kotlinsimpleappfindmyage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        /*btn_Enter.setOnClickListener {
+            val yearOfBirth:Int = txt_inputAge.text.toString().toInt()
+            val myAge = 2017-yearOfBirth
+            tv_showAge.text = "Your Age is $myAge"
+        }*/
+    }
+
+    //set onclick on btn_Enter to select use method
+    fun btn_FindAge(view:View){
+        val yearOfBirth:Int = txt_inputAge.text.toString().toInt()
+        val myAge = 2017-yearOfBirth
+        tv_showAge.text = "Your Age is $myAge"
+    }
+}
